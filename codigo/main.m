268934cacaa55b25596fc60366de7d3c9c1d56e2
Francisco Fernandes <francisco201262@gmail.com>
2022-03-27 22:01:53 +0100

    data visualization

--- codigo/main.m
+++ codigo/main.m
@@ -1,4 +1,5 @@
-close all
+close all;
+clear;
 %4.1 Data import
 xls_data = readmatrix('..\dataset\heart_2020_cleaned.csv');
 
@@ -15,17 +16,29 @@ data.name='Heart Diseases dataset';
 data.indep_names=["_BMI5"	"Smoking"	"AlcoholDrinking"	"Stroke"	"PhysicalHealth"	"MentalHealth"	"DiffWalking"	"Sex"	"AgeCategory"	"Race"	"Diabetic"	"PhysicalActivity"	"GenHealth"	"SleepTime"	"Asthma"];
 data.dep_names=["CoronaryHeartDisease"	"MyocardialInfarction"	"KidneyDisease"	"SkinCancer"];
 
+%scenario A CoronaryHeartDisease:0/1
+data.y=data.y(1,:)-1;
+
 %4.2 Feature Selection and Reduction
 
 %4.2.1 Scaling
 data=scalestd(data);
 
+%data visualization
 figure('Name','Data Distribution');
 for i=1:data.dim
     subplot(3,5,i);
     histogram(data.X(i,:),10);
     title(data.indep_names(i));
 end
+
+figure('Name','Data Distribution per Class');
+for i=1:data.dim
+    subplot(3,5,i);
+    boxplot(data.X(i,:),data.y);
+    title(data.indep_names(i));
+end
+
 
 %4.2.1 PCA
 model = pca(data.X);
@@ -44,6 +57,19 @@ end
 subplot(1,2,2);
 bar(variance_preserved);
 title("Variance Preserved");
+
+%Kruscal wallis and correlation coefficients matrix 
+for i=1:data.dim
+    [p,atab,stats]=kruskalwallis(data.X(i,:),data.y,'off');
+    rank{i,1}=data.indep_names{i};
+    rank{i,2}=atab{2,5};
+    atab
+end
+
+C=corrcoef(data.X');
+figure('Name',"Correlation Coefficients Matrix");
+heatmap(C);
+title("Correlation Coefficients Matrix");
 %4.3 Experimental Analysis
 %4.4 Pattern Recognition Methods
 
