be978d8ab76a3eb1120634fe06c79c59a16670ac
Francisco Fernandes <francisco201262@gmail.com>
2022-05-17 21:48:57 +0100

    slight adjustments to chooseScenario

--- codigo/chooseScenario.m
+++ codigo/chooseScenario.m
@@ -1,40 +1,23 @@
-function [data_train,data_test] = chooseScenario(nSplit, nScenario)
-
-data=load('data.mat').data;
-[data_train,data_test]=splitDataset(data,nSplit);
-
-
-%scenario A - CoronaryHeartDisease: 0/1
-if nScenario == 1
-    data_train.y=data_train.y(1,:)-1;
-    data_test.y=data_test.y(1,:)-1;
-end
-%scenario B - HeartDisease: 0/1
-if nScenario == 2
-    data_train.y=data_train.y(1:2,:)-1;
-    index = find(data_train.y(1,:) == 1  & data_train.y(2,:) == 1);
-    data_train.y = zeros(1, data_train.num_data);
-    data_train.y(index) = 1;
-
-    data_test.y=data_test.y(1:2,:)-1;
-    index = find(data_test.y(1,:) == 1  & data_test.y(2,:) == 1);
-    data_test.y = zeros(1, data_test.num_data);
-    data_test.y(index) = 1;
-end
-if nScenario == 3
-    data_train.y=data_train.y(1:4,:)-1;
-    index_noHeartDiseases = find(data_train.y(1,:) == 1  & data_train.y(2,:) == 1);
-    index_HeartDiseases_noComorbidities = find((data_train.y(1,:) == 0  | data_train.y(2,:) == 0) & (data_train.y(3,:) == 1 | data_train.y(4,:) == 1));
-    data_train.y = zeros(1, data_train.num_data);
-    data_train.y(index_noHeartDiseases) = 2;
-    data_train.y(index_HeartDiseases_noComorbidities) = 1;
-
+function [data] = chooseScenario(data, nScenario)
     
-    data_test.y=data_test.y(1:4,:)-1;
-    index_noHD = find(data_test.y(1,:) == 1  & data_test.y(2,:) == 1);
-    index_HD_noC = find((data_test.y(1,:) == 0  | data_test.y(2,:) == 0) & (data_test.y(3,:) == 1 | data_test.y(4,:) == 1));
-    data_test.y = zeros(1, data_test.num_data);
-    data_test.y(index_noHD) = 2;
-    data_test.y(index_HD_noC) = 1;
-end
+    %scenario A - CoronaryHeartDisease: 1-Positive/2-Negative
+    if nScenario == 1
+        data.y=data.y(1,:);
+    end
+    %scenario B - HeartDisease: 1-Positive (CoronaryHeartDisease or MyocardialInfarction)/2-Negative
+    if nScenario == 2
+        index = find(data.y(1,:) == 1  | data.y(2,:) == 1);
+        data.y = ones(1, data.num_data).*2;
+        data.y(index) = 1;
+    end
+    
+    %scenario B - Heart Disease with comorbidities
+    if nScenario == 3
+        index_noHeartDiseases = find(data.y(1,:) == 2  & data.y(2,:) == 2);
+        index_HeartDiseases_noComorbidities = find((data.y(1,:) == 1  | data.y(2,:) == 1) & data.y(3,:) == 2 & data.y(4,:) == 2);
+        data.y = ones(1, data.num_data);
+        data.y(index_HeartDiseases_noComorbidities) = 2;
+        data.y(index_noHeartDiseases) = 3;
+    end
+
 end
